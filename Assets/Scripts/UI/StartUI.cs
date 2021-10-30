e9704712b36f1607fef6b3417cffb7123f5935c8
deoignition <deoignition@gmail.com>
2021-10-29 22:55:00 -0400

    Start Screen Added
    - Added start scene and main menu screen
    - Renamed main scene from samplescene.unity to main.unity
    - Imported TMPro package

---
+++ Assets/Scripts/UI/StartUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Class used to control the UI at the start of the game
+public class StartUI : UI
+{
+    //Method called when the user clicks to start
+    public void OnClickStart()
+    {
+        //Loads the main scene
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+
+    //Method called when the user clicks to quit
+    public void OnClickQuit()
+    {
+        //Quits the application, doesn't work in the editor so a debug is added to validate that it is working
+        Application.Quit();
+        Debug.LogWarning("Application could not quit because it is being run in the editor");
+    }
+}
---
+++ Assets/Scripts/UI/UI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Inherited base class, used for all UI in the game
+public abstract class UI : MonoBehaviour
+{
+    //Gameobject which contains all the UI elements, allows for the UI to be disabled while this script is still active
+    [SerializeField] private GameObject container;
+
+    //Overridable method called to show the UI
+    public virtual void Show()
+    {
+        //Activates the container
+        container.SetActive(true);
+    }
+
+    //Overridable method called to hide the UI
+    public virtual void Hide()
+    {
+        //Deactivates the container
+        container.SetActive(false);
+    }
+}

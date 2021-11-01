265fe0c6e6d7a791972b80952066060b39245fc8
deoignition <93416177+AustinBray77@users.noreply.github.com>
2021-11-01 17:47:30 -0400

    Pick Up Item Glow Added
    A basic version of the pick up item glow has been added.

--- Assets/Scripts/CollectableItem.cs
+++ Assets/Scripts/CollectableItem.cs
@@ -6,10 +6,12 @@ public class CollectableItem : MonoBehaviour
 {
     public string itemName;
 
+    private SpriteRenderer _renderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -17,4 +19,7 @@ public class CollectableItem : MonoBehaviour
     {
         
     }
+
+    public void SetIntensity(float intensity) =>
+        _renderer.material.SetColor("_EmissionColor", Color.white * intensity);
 }
--- Assets/Scripts/playerInventoryManager.cs
+++ Assets/Scripts/playerInventoryManager.cs
@@ -6,15 +6,18 @@ public class PlayerInventoryManager : MonoBehaviour
 {
     [SerializeField] GameObject playerInventoryUI; //reference to the inventory UI, so the player can send game objects that they collect to the inventory display
 
+    [SerializeField] float bloomIntensity; // how much the closest object should glow
     [SerializeField] float pickUpDistance; //how close the player needs to be to objects to pick them up
+    private LayerMask player;
     CircleCollider2D pickUpTrigger; //trigger collider that can be used to detect objects within a certain radius
 
     float closestObjectDistance;
-    GameObject closestObject = null;
+    GameObject closestObject = null, prevObj = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        player = LayerMask.GetMask("Player");
         pickUpTrigger = GetComponent<CircleCollider2D>();
         pickUpTrigger.radius = pickUpDistance;
     }
@@ -22,19 +25,26 @@ public class PlayerInventoryManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Temp
+        pickUpTrigger.radius = pickUpDistance;
     }
 
     void FixedUpdate()
     {
+        if(prevObj != null)
+        {
+            prevObj.GetComponent<CollectableItem>().SetIntensity(1);
+        }
+
         //Do something with the closest object
         if (closestObject != null)
         {
-
+            closestObject.GetComponent<CollectableItem>().SetIntensity(bloomIntensity);
         }
 
         //Resets variables at the end of fixed update before onTrigger functions are called
         closestObjectDistance = pickUpDistance;
+        prevObj = closestObject;
         closestObject = null;
     }
 
@@ -64,7 +74,11 @@ public class PlayerInventoryManager : MonoBehaviour
             //Performs a raycast to make sure that you have direct line of sight with the object
             //Stops you from collecting items through walls
             Vector2 direction = ((Vector2)(other.transform.position - transform.position)).normalized;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, pickUpDistance);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, pickUpDistance, ~player);
+
+            Debug.DrawRay(transform.position, direction * pickUpDistance);
+
+            Debug.Log(hit.collider.gameObject.name);
 
             if (hit.collider.gameObject == other.gameObject)
             {

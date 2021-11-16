0eb9c54ba1843ac3e06b99453493fe2a8026bb4a
raisssu <rizoan789@gmail.com>
2021-11-15 22:17:09 -0500

    Added pause functionality

--- Assets/Scripts/UI/PauseUI.cs
+++ Assets/Scripts/UI/PauseUI.cs
@@ -15,8 +15,8 @@ public class PauseUI : UI
     // Update is called once per frame
     void Update()
     {
-        //Pauses the game if escape is pressed
-        if (Input.GetKey(KeyCode.Escape)){
+        //Pauses and unpauses the game if escape is pressed
+        if (Input.GetKeyDown(KeyCode.Escape)){
             isPaused = !isPaused;
             if (isPaused)
             {
@@ -28,4 +28,44 @@ public class PauseUI : UI
             }
         }
     }
+
+    //Overriding the method to show ui
+    public override void Show()
+    {
+        //Activates the container
+        container.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        //Time will stop when game is paused
+    }
+    
+    
+
+    //Overriding the method to hide ui
+    public override void Hide()
+    {
+        //Deactivates the container
+        container.SetActive(false);
+        Time.timeScale = 1f;
+        //Time will continue when unpaused
+
+        isPaused = false;
+        
+        
+    }
+
+    //temporary method, can be used when menu ui is created
+    public void LoadMenu()
+    {
+        Debug.Log("menu");
+    }
+
+    //method quits the game *nothing happens in unity editor but should quit if the game is built and run*
+    public void QuitGame()
+    {
+        Debug.Log("quitting");
+        Application.Quit();
+        
+    }
+
 }

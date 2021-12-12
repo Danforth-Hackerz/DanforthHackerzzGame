0df017ad445d6120f1392810e1e8119cd85c06d9
deoignition <93416177+AustinBray77@users.noreply.github.com>
2021-12-12 00:11:47 -0500

    Recreated Reference.cs and added comments

--- Assets/Scripts/Refrence.cs
+++ Assets/Scripts/Refrence.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Class to store static refrences to obejcts in the scene
 public class Reference : MonoBehaviour
 {
+    //In scene refrences
     [SerializeField] private PlayerInventoryManager _PIM;
 
+    //Static refrences
     public static PlayerInventoryManager PIM;
 
+    //Method called on scene load
     private void Start()
     {
+        //Assigns static refrences to the in scene refrences
         PIM = _PIM;
     }
 }

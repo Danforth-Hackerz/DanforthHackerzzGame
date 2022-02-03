00a74a4410a92de85995f0543acd2e6f7d7484a8
deoignition <93416177+AustinBray77@users.noreply.github.com>
2022-02-02 22:12:01 -0500

    Update TimedKeyPress.cs

--- Assets/Scripts/TimedKeyPress.cs
+++ Assets/Scripts/TimedKeyPress.cs
@@ -6,11 +6,20 @@ using UnityEngine.UI;
 [Serializable]
 public class TimedKeyPress
 {
+    public enum ObstacleType
+    {
+        JumpOver,
+        JumpUp,
+        Slide,
+        Drop
+    }
+
     private static float time = 1f;
     private static float timeBuffer = 0.125f;
 
     public KeyCode keyCode;
     public Vector3 triggerPosition;
+    public ObstacleType obstacleType;
 
     public IEnumerator Show(Text text, GameObject slider, Animator animator, Action<bool> callback)
     {

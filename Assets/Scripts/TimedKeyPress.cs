955f8386926d4f593e1a86493103257f59b9a485
deoignition <93416177+AustinBray77@users.noreply.github.com>
2022-02-02 22:08:24 -0500

    Update TimedKeyPress.cs

--- Assets/Scripts/TimedKeyPress.cs
+++ Assets/Scripts/TimedKeyPress.cs
@@ -9,13 +9,15 @@ public class TimedKeyPress
     private static float time = 1f;
     private static float timeBuffer = 0.125f;
 
-    [SerializeField] public KeyCode keyCode { get; private set; }
-    [SerializeField] public Vector3 triggerPosition { get; private set; }
+    public KeyCode keyCode;
+    public Vector3 triggerPosition;
 
-    public IEnumerator Show(Text text, Slider slider, Action<bool> callback)
+    public IEnumerator Show(Text text, GameObject slider, Animator animator, Action<bool> callback)
     {
         //Animate the bar and text and wait the designated time
+        slider.gameObject.SetActive(true);
 
+        text.text = keyCode.ToString();
         yield return new WaitForSeconds(time - timeBuffer);
 
         //Set the text to glow and wait for input

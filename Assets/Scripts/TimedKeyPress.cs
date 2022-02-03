2149c0fa2ad416d3859e209fcaf1e79e30e1a12f
deoignition <93416177+AustinBray77@users.noreply.github.com>
2022-02-02 21:54:55 -0500

    Added positions to timed key press

--- Assets/Scripts/TimedKeyPress.cs
+++ Assets/Scripts/TimedKeyPress.cs
@@ -10,23 +10,27 @@ public class TimedKeyPress
     private static float timeBuffer = 0.125f;
 
     [SerializeField] private KeyCode keyCode;
+    [SerializeField] private Vector3 triggerPosition;
 
-    public IEnumerator Show(Text text, Scrollbar sc, Action callback = null)
+    public IEnumerator Show(Text text, Slider slider, Action<bool> callback)
     {
-        //Animate the text
-        {
-            float counter = 0;
+        //Animate the bar and text and wait the designated time
 
-            while (counter < time)
+        yield return new WaitForSeconds(time - timeBuffer);
+
+        //Set the text to glow and wait for input
+        float counter = 0;
+
+        while (counter <= timeBuffer)
+        {
+            if (Input.GetKey(keyCode))
             {
-                //Animate bar
-
-                counter += Time.deltaTime;
-                yield return new WaitForEndOfFrame();
+                callback(true);
+                yield break;
             }
-        }
-        {
-            float counter = 0;
+
+            counter += Time.deltaTime;
+            yield return null;
         }
 
         yield return null;
